f764407978f995bf49bf671aa348d291be1eefb0
justDare <dsampare04@gmail.com>
2019-01-30 18:34:55 -0800

    initial commit

---
+++ java-2/shapes/Shape.java
@@ -0,0 +1,27 @@
+//Darian Sampare
+//T00578132
+//COMP 1232
+//Y.Linn
+//Assignment 1
+//*************************************************************************
+
+
+
+//creates an initial 3d shape with some variables and methods to be used by each child class
+public class Shape
+{
+
+	public double height = 0;
+	public double area = 0;
+	public double volume = 0;
+
+	public double getSurfaceArea()
+	{
+		return 0;
+	}
+
+	public double getVolume()
+	{
+		return 0;
+	}
+}
