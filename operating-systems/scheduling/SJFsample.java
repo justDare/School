7dfcde56ab388b6972915e07b0514bea8a6f98f9
Darian Sampare <dsampare04@gmail.com>
2019-03-21 09:20:38 -0700

    bug in turn/wait calculations for part 2

--- operating-systems/scheduling/SJFsample.java
+++ operating-systems/scheduling/SJFsample.java
@@ -3,7 +3,7 @@ public class SJFsample {
 
 		PriorityQueue readyQ = new PriorityQueue();
 		ProcessControlBlock pcb;
-		ProcessControlBlock pcbPrev = new ProcessControlBlock(0,0,0,0);
+		ProcessControlBlock pcbPrev = new ProcessControlBlock(0, 0, 0, 0);
 		SampleReader sr;
 		String sampleFile;
 		int process, arrival, priority, burst;
@@ -12,13 +12,13 @@ public class SJFsample {
 		sampleFile = KeyboardIn.readLineWord();
 		sr = new SampleReader(sampleFile);
 
-		while(true) {
-			
+		while (true) {
+
 			process = -1;
 			arrival = -1;
 			priority = -1;
 			burst = -1;
-			
+
 			process = sr.readProcess();
 			if (process < 0)
 				break;
@@ -34,16 +34,18 @@ public class SJFsample {
 
 			pcb = new ProcessControlBlock(process, arrival, priority, burst);
 			readyQ.putQueue(pcb, arrival);
-				
+
 		}
 
-		int [] completions = new int[readyQ.noItems];
-        int turnAround = 0;
+		int[] completions = new int[readyQ.noItems];
+		int turnAround = 0;
 		int waitTime = 0;
+
+		int thisWait, thisTurn;
 
 		System.out.println(readyQ.noItems);
 
-		// counters for minutes 
+		// counters for minutes
 		int oneMinute = 0;
 		int minuteCounter = 0;
 
@@ -54,140 +56,162 @@ public class SJFsample {
 		boolean startOneMinute = false;
 
 		int processNum = 0;
-        int processArrival = 0;
-        int processPriority = 0;
-        int processBurst = 0;
-	
+		int processArrival = 0;
+		int processPriority = 0;
+		int processBurst = 0;
 
 		count = intervalCount = 0;
-		while(!readyQ.isEmpty()) {
+		while (!readyQ.isEmpty()) {
 
 			// get entry off the queue
-            pcb = (ProcessControlBlock)readyQ.getQueue();
-            processNum = pcb.getProcessNo();
-            processArrival = pcb.getArrivalTime();
+			pcb = (ProcessControlBlock) readyQ.getQueue();
+			processNum = pcb.getProcessNo();
+			processArrival = pcb.getArrivalTime();
 			processBurst = pcb.getCpuBurstTime();
 
-            //  first process has no wait time
-            if (processNum == 0) {
+			// first process has no wait time
+			if (processNum == 0) {
 
 				// get values for current process
 				completions[count] = processArrival + processBurst;
 				turnAround += completions[count] - processArrival;
-				waitTime += (completions[count] - processArrival) - processBurst;
 
 				count++;
 				intervalCount++;
 
-				System.out.println("completed " + completions[count-1]);
-				System.out.println("turn: "+ turnAround);
+				System.out.println("completed " + completions[count - 1]);
+				System.out.println("turn: " + turnAround);
 				System.out.println("wait: " + waitTime);
 
-			// second process can never be swapped with first 
+				// second process can never be swapped with first
 			} else if (processNum == 1) {
 
 				// store as the previous process
 				pcbPrev = pcb;
 
-			// all procceses after 0 and 1
+				// all procceses after 0 and 1
 			} else {
 
-				// can only swap if the previous process has not started running yet, and if the new burst time is lower
-				if (processArrival < completions[count-1] && processBurst < pcbPrev.cpuBurstTime) {
-					
-					completions[count] = completions[count-1] + processBurst;
-					turnAround += (completions[count] - processArrival);
-					waitTime += ((completions[count] - processArrival) - processBurst);
+				// can only swap if the previous process has not started running yet, and if the
+				// new burst time is lower
+				if (processArrival < completions[count - 1] && processBurst < pcbPrev.cpuBurstTime) {
+
+					// wait,completion, and turnaround time of executed process
+					thisWait = completions[count - 1] - processArrival;
+					completions[count] = processArrival + thisWait + processBurst;
+					thisTurn = completions[count] - arrival;
+
+					waitTime += thisWait;
+					turnAround += thisTurn;
 
 					// if last item is popped, execute the swapped process
-					if(processNum == completions.length-1) {
+					if (processNum == completions.length - 1) {
 
-						completions[count+1] = completions[count] + pcbPrev.cpuBurstTime;
-						turnAround += (completions[count+1] - pcbPrev.arrivalTime);
-						waitTime += ((completions[count+1] - processArrival) - pcbPrev.cpuBurstTime);
+						// wait,completion, and turnaround time of executed process
+						thisWait = completions[count - 1] - pcbPrev.arrivalTime;
+						completions[count] = pcbPrev.arrivalTime + thisWait + pcbPrev.cpuBurstTime;
+						thisTurn = completions[count] - pcbPrev.arrivalTime;
+
+						waitTime += thisWait;
+						turnAround += thisTurn;
 
 					}
 
 					System.out.println("if");
+					System.out.println("burst: " + processBurst);
+					System.out.println("arrival: " + processArrival);
+					System.out.println("----------------------------");
 
-				// else execute the previously popped
+					// else execute the previously popped
 				} else {
 
-					completions[count] = completions[count-1] + pcbPrev.cpuBurstTime;
-					turnAround += (completions[count] - pcbPrev.arrivalTime);
-					waitTime += ((completions[count] - processArrival) - pcbPrev.cpuBurstTime);
+					// wait,completion, and turnaround time of executed process
+					thisWait = completions[count - 1] - pcbPrev.arrivalTime;
+					completions[count] = pcbPrev.arrivalTime + thisWait + pcbPrev.cpuBurstTime;
+					thisTurn = completions[count] - pcbPrev.arrivalTime;
 
+					waitTime += thisWait;
+					turnAround += thisTurn;
+
+					// store the moset recent pop for next comparison
 					pcbPrev = pcb;
 
-					// if last item is popped, execute the swapped process
-					if(processNum == completions.length-1) {
+					// if last item is popped, execute the current process
+					if (processNum == completions.length - 1) {
 
-						completions[count+1] = completions[count] + processBurst;
-						turnAround += (completions[count+1] - processArrival);
-						waitTime += ((completions[count+1] - processArrival) - processBurst);
+						// wait,completion, and turnaround time of executed process
+						thisWait = completions[count - 1] - processArrival;
+						completions[count] = processArrival + thisWait + processBurst;
+						thisTurn = completions[count] - arrival;
+
+						waitTime += thisWait;
+						turnAround += thisTurn;
 
 					}
 
 					System.out.println("else:");
+					System.out.println("arrival: " + pcbPrev.arrivalTime);
+					System.out.println("burst: " + pcbPrev.cpuBurstTime);
+					System.out.println("----------------------------");
 
 				}
 
 				count++;
 				intervalCount++;
 
-				System.out.println("completed " + completions[count-1]);
-				System.out.println("turn: "+ turnAround);
+				System.out.println("completed " + completions[count - 1]);
+				System.out.println("turn: " + turnAround);
 				System.out.println("wait: " + waitTime);
+				System.out.println("----------------------------");
 
 			}
-			
-			 // print stats after the 10 minute(600000ms)mark
-			 if(completions[count] >= 600000 && !startOneMinute)  {
-                
-                System.out.println("10 minute mark");
-                System.out.println("------------------------------------------------------------");
-                System.out.println("Average turn around time for past 10 minutes: " + turnAround/intervalCount);
-                System.out.println("Average wait time for past 10 minutes: " + waitTime/intervalCount);
-                System.out.println();
 
-                // start printing each minute
-                startOneMinute = true;
+			// print stats after the 10 minute(600000ms)mark
+			if (completions[count] >= 600000 && !startOneMinute) {
 
-                minuteCounter = 10;
+				System.out.println("10 minute mark");
+				System.out.println("------------------------------------------------------------");
+				System.out.println("Average turn around time for past 10 minutes: " + turnAround / intervalCount);
+				System.out.println("Average wait time for past 10 minutes: " + waitTime / intervalCount);
+				System.out.println();
 
-                // reset totals
-                turnAround = 0;
-                waitTime= 0;
-                intervalCount = 0;
+				// start printing each minute
+				startOneMinute = true;
 
-                // remainder is added to minute counter
-                oneMinute = completions[count] - 600000;
-            
-            // once one minute intervals begin
-            } else if (startOneMinute) {
-                
-                // add to minute counter
-				oneMinute += (completions[count] - completions[count-1]);
-				
-                // once minute is up, print stats and reset counter
-                if(oneMinute >= 60000) {
+				minuteCounter = 10;
 
-                    minuteCounter++;
+				// reset totals
+				turnAround = 0;
+				waitTime = 0;
+				intervalCount = 0;
 
-                    System.out.println(minuteCounter + " minute mark");
-                    System.out.println("------------------------------------------------------------");
-                    System.out.println("Average turn around time for past 10 minutes: " +  turnAround/intervalCount);
-                    System.out.println("Average wait time for past 10 minutes: " +  waitTime/intervalCount);
-                    System.out.println();
+				// remainder is added to minute counter
+				oneMinute = completions[count] - 600000;
 
-                    oneMinute = oneMinute - 60000;
-                   
+				// once one minute intervals begin
+			} else if (startOneMinute) {
 
-                    // reset totals
-                    turnAround = 0;
-                    waitTime = 0;
-                    intervalCount = 0;
-                }
+				// add to minute counter
+				oneMinute += (completions[count] - completions[count - 1]);
+
+				// once minute is up, print stats and reset counter
+				if (oneMinute >= 60000) {
+
+					minuteCounter++;
+
+					System.out.println(minuteCounter + " minute mark");
+					System.out.println("------------------------------------------------------------");
+					System.out.println("Average turn around time for past 10 minutes: " + turnAround / intervalCount);
+					System.out.println("Average wait time for past 10 minutes: " + waitTime / intervalCount);
+					System.out.println();
+
+					oneMinute = oneMinute - 60000;
+
+					// reset totals
+					turnAround = 0;
+					waitTime = 0;
+					intervalCount = 0;
+				}
 			}
 
 		}
